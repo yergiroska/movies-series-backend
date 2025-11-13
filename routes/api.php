5e8fb66c4ba5b0fefaa70fc8f7d39ec6929233bc
Yergiroska Aguirre <yergiroska77@gmail.com>
2025-11-13 12:32:18 +0100

    feat: Integración completa con API de TMDB
    - Crear TmdbService para conectar con TMDB API
    - Crear MovieController con endpoints de películas
    - Crear TVController con endpoints de series
    - Configurar rutas API para /movies y /tv
    - Agregar configuración TMDB en config/tmdb.php
    - Probar todos los endpoints con Insomnia
    CLOSE #3

--- routes/api.php
+++ routes/api.php
@@ -46,3 +46,23 @@ Route::middleware('auth:sanctum')->group(function () {
     // Usuario autenticado
     Route::get('/user', [AuthController::class, 'user']);
 });
+
+// Rutas de Películas (públicas)
+Route::prefix('movies')->group(function () {
+    Route::get('/popular', [App\Http\Controllers\Api\MovieController::class, 'popular']);
+    Route::get('/top-rated', [App\Http\Controllers\Api\MovieController::class, 'topRated']);
+    Route::get('/upcoming', [App\Http\Controllers\Api\MovieController::class, 'upcoming']);
+    Route::get('/search', [App\Http\Controllers\Api\MovieController::class, 'search']);
+    Route::get('/{id}', [App\Http\Controllers\Api\MovieController::class, 'show']);
+    Route::get('/{id}/similar', [App\Http\Controllers\Api\MovieController::class, 'similar']);
+});
+
+// Rutas de Series (públicas)
+Route::prefix('tv')->group(function () {
+    Route::get('/popular', [App\Http\Controllers\Api\TVController::class, 'popular']);
+    Route::get('/top-rated', [App\Http\Controllers\Api\TVController::class, 'topRated']);
+    Route::get('/on-the-air', [App\Http\Controllers\Api\TVController::class, 'onTheAir']);
+    Route::get('/search', [App\Http\Controllers\Api\TVController::class, 'search']);
+    Route::get('/{id}', [App\Http\Controllers\Api\TVController::class, 'show']);
+    Route::get('/{id}/similar', [App\Http\Controllers\Api\TVController::class, 'similar']);
+});
